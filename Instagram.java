971f3915dd025d6e5f08fd11be5ad84e7d033607
Rupesh <srupesh51@gmail.com>
2018-05-16 17:48:48 +0530

    Finished LLD for Instagram System

--- Instagram.java
+++ Instagram.java
@@ -1,183 +1,233 @@
 import java.util.*;
 class Picture {
   public String pictureName;
-  public String picture;
-  Picture(String pictureName,
-  String picture){
-    this.pictureName = pictureName;
-    this.picture = picture;
+  public User toUser;
+  Picture(User receiver, String picture){
+    this.toUser = receiver;
+    this.pictureName = picture;
   }
 }
 class User {
-  private int userId;
-  private String userName;
-  private String firstName;
-  private String lastName;
   private String email;
-  private HashMap<Integer, User> map;
-  private HashMap<Integer, LinkedList<User>> map1;
-  private HashMap<Integer, LinkedList<Picture>> map2;
+  private String fullName;
+  private static HashMap<String,LinkedList<Friend>> friends = new HashMap<String,LinkedList<Friend>>();
+  private static HashMap<String,LinkedList<Picture>> pictures = new HashMap<String,LinkedList<Picture>>();
   User(){
-    map = new HashMap<Integer, User>();
-    map1 = new HashMap<Integer, LinkedList<User>>();
-    map2 = new HashMap<Integer, LinkedList<Picture>>();
   }
-  User(String userName, String firstName,
-  String lastName, String email){
-    this.userName = userName;
-    this.firstName = firstName;
-    this.lastName = lastName;
+  User(String email, String fullName){
     this.email = email;
+    this.fullName = fullName;
   }
-  public void addUser(User user){
-    synchronized(this){
-      int id = user.getUserId();
-      if(id == 0 && !map.containsKey(map.size()+1)){
-        user.userId = map.size()+1;
-        map.put(map.size()+1, user);
+  public LinkedList<Picture> getPictures(String email){
+    return this.pictures.containsKey(email) ? this.pictures.get(email) : null;
+  }
+  public LinkedList<Friend> getFriends(String email){
+    return this.friends.containsKey(email) ? this.friends.get(email) : null;
+  }
+  public String getFullName(){
+    return this.fullName;
+  }
+  public String getEmail(){
+    return this.email;
+  }
+  public void addFriend(User curr, User user){
+    LinkedList<Friend> fl = null;
+    if(!this.friends.containsKey(curr.getEmail())){
+      fl = new LinkedList<Friend>();
+      fl.add(new Friend(user));
+      this.friends.put(curr.getEmail(),fl);
+    } else {
+      fl = this.friends.get(curr.getEmail());
+      fl.add(new Friend(user));
+      this.friends.put(curr.getEmail(),fl);
+    }
+  }
+  public void removeFriend(User curr, User user){
+    LinkedList<Friend> fl = null;
+    if(this.friends.containsKey(curr.getEmail())){
+      fl = this.friends.get(curr.getEmail());
+      for(int i = 0; i < fl.size(); i++){
+        if(fl.get(i).getFriend() == user){
+          fl.remove(i);
+          break;
+        }
+      }
+      this.friends.put(curr.getEmail(),fl);
+    }
+  }
+  public void addPicture(User user, String pic){
+    LinkedList<Picture> fl = null;
+    if(!this.pictures.containsKey(user.getEmail())){
+      fl = new LinkedList<Picture>();
+      fl.add(new Picture(user, pic));
+      this.pictures.put(user.getEmail(), fl);
+    } else {
+      fl = this.pictures.get(user.getEmail());
+      fl.add(new Picture(user, pic));
+      this.pictures.put(this.getEmail(), fl);
+    }
+  }
+  public void removePicture(User user, String pic){
+    if(!this.pictures.containsKey(user.getEmail())){
+      return;
+    } else {
+      LinkedList<Picture> pics = this.pictures.get(user.getEmail());
+      for(int i = 0; i < pics.size(); i++){
+        Picture pic1 = pics.get(i);
+        if(pic1.pictureName == pic){
+          pics.remove(i);
+          break;
+        }
+      }
+      this.pictures.put(user.getEmail(), pics);
+    }
+  }
+  public void addPicture(User user, User u2, String pic,
+  boolean visited[], int i){
+    visited[i] = true;
+    LinkedList<Picture> fl = null;
+    if(!this.pictures.containsKey(user.getEmail())){
+      fl = new LinkedList<Picture>();
+      fl.add(new Picture(u2, pic));
+      this.pictures.put(user.getEmail(), fl);
+    } else {
+      fl = this.pictures.get(user.getEmail());
+      fl.add(new Picture(u2, pic));
+      this.pictures.put(this.getEmail(), fl);
+    }
+    if(this.friends.containsKey(u2.getEmail())){
+      LinkedList<Friend> fl1 = this.friends.get(u2.getEmail());
+      for(int j = 0; j < fl1.size(); j++){
+        User u1 = fl1.get(j).getFriend();
+        if(!visited[j]){
+          this.addPicture(user,u1,pic,visited,j);
+        }
       }
     }
   }
-  public int getUserId() {
-    return userId;
-  }
-  public void addPicture(User user, Picture picture){
-    synchronized(this){
-      int id = user.getUserId();
-      if(id == 0){
-        return;
+  public void removePicture(User user, User u3, String pic,
+  boolean visited[], int i){
+    visited[i] = true;
+    if(!this.pictures.containsKey(user.getEmail())){
+      return;
+    } else {
+      LinkedList<Picture> picturesList = this.pictures.get(user.getEmail());
+      for(int k = 0; k < picturesList.size(); k++){
+        Picture p = picturesList.get(k);
+        if(p.pictureName == pic){
+          picturesList.remove(k);
+        }
       }
-      LinkedList<Picture> u2 = null;
-      if(!map2.containsKey(id)){
-        u2 = new LinkedList<Picture>();
-        u2.add(picture);
-        map2.put(id, u2);
-      } else {
-        u2 = map2.get(id);
-        u2.add(picture);
-        map2.put(id, u2);
-      }
-      update(friendList(user), picture);
+      this.pictures.put(user.getEmail(), picturesList);
     }
-  }
-  public void addFriend(User user, User friend){
-    synchronized(this){
-      int id = user.getUserId();
-      if(id == 0){
-        return;
-      }
-      LinkedList<User> u2 = null;
-      if(!map1.containsKey(id)){
-        u2 = new LinkedList<User>();
-        u2.add(friend);
-        map1.put(id, u2);
-      } else {
-        u2 = map1.get(id);
-        u2.add(friend);
-        map1.put(id, u2);
+    if(!this.friends.containsKey(user.getEmail())){
+      return;
+    } else {
+    LinkedList<Friend> fl = this.friends.get(user.getEmail());
+    for(int j = 0; j < fl.size(); j++){
+        User u1 = fl.get(j).getFriend();
+        if(!visited[j]){
+          this.removePicture(user,u1,pic,visited,j);
+        }
       }
     }
   }
-  public void update(LinkedList<User> u2, Picture picture){
-    synchronized(this){
-      if(u2 == null){
-        return;
+}
+class Friend {
+  private User user;
+  Friend(User user){
+    this.user = user;
+  }
+  public User getFriend(){
+    return this.user;
+  }
+}
+class InstagramSystem {
+  private LinkedList<User> userList;
+  private static User userInstance = new User();
+  InstagramSystem(){
+    userList = new LinkedList<User>();
+  }
+  public void addUser(String email, String fullName){
+    userList.add(new User(email, fullName));
+  }
+  public User getUser(String email){
+    LinkedList<User> ul = this.userList;
+    for(int i = 0; i < ul.size(); i++){
+      if(email == ul.get(i).getEmail()){
+        return ul.get(i);
       }
-      update(u2,0,picture);
+    }
+    return null;
+  }
+  public void removeUser(User user){
+    LinkedList<User> ul = this.userList;
+    for(int i = 0; i < ul.size(); i++){
+      if(user == ul.get(i)){
+        ul.remove(i);
+      }
     }
   }
-  public void update(LinkedList<User> u2, int i, Picture picture){
-    synchronized(this){
-      if(i >= u2.size()){
-        return;
-      }
-      int id = u2.get(i).getUserId();
-      if(id == 0){
-        return;
-      }
-      LinkedList<Picture> u21 = null;
-      if(!map2.containsKey(id)){
-        u21 = new LinkedList<Picture>();
-        u21.add(picture);
-        map2.put(id, u21);
-      } else {
-        u21 = map2.get(id);
-        u21.add(picture);
-        map2.put(id, u21);
-      }
-      update(u2,i+1,picture);
+  public void addFriend(User curr, User u){
+    userInstance.addFriend(curr,u);
+  }
+  public void removeFriend(User curr, User u){
+    userInstance.removeFriend(curr,u);
+  }
+  public void addPicture(User user, String pic){
+    userInstance.addPicture(user,pic);
+    if(userInstance.getFriends(user.getEmail()) == null){
+      return;
+    }
+    LinkedList<Friend> l1 = userInstance.getFriends(user.getEmail());
+    boolean visited[] = new boolean[userList.size()];
+    for(int i = 0; i < l1.size(); i++){
+        if(!visited[i]){
+          User u2 = l1.get(i).getFriend();
+          userInstance.addPicture(user, u2, pic, visited, i);
+       }
     }
   }
-  public void getPictures(User u){
-    synchronized(this){
-      if(!map2.containsKey(u.getUserId())){
-        return;
+  public void removePicture(User user, String pic){
+    userInstance.removePicture(user, pic);
+    if(userInstance.getFriends(user.getEmail()) == null){
+      return;
+    }
+    LinkedList<Friend> l1 = userInstance.getFriends(user.getEmail());
+    boolean visited[] = new boolean[userList.size()];
+    for(int i = 0; i < l1.size(); i++){
+      if(!visited[i]){
+        User u2 = l1.get(i).getFriend();
+        userInstance.removePicture(user, u2, pic, visited, i);
       }
-      getPictures(map2.get(u.getUserId()),0);
     }
   }
-  public void getPictures(LinkedList<Picture> u2, int i){
-    synchronized(this){
-      if(i >= u2.size()){
-        return;
-      }
-      Picture p = u2.get(i);
-      System.out.print(p.pictureName+" "+p.picture+" ");
-      getPictures(u2, i+1);
+  public void showPicture(User user){
+    LinkedList<Picture> pics = userInstance.getPictures(user.getEmail());
+    if(pics == null){
+      return;
     }
-  }
-  public void removeFriend(User user, User friend){
-    synchronized(this){
-      int id = user.getUserId();
-      if(id == 0){
-        return;
-      }
-      removeFriend(friendList(user),0,friend);
+    for(int i = 0; i < pics.size(); i++){
+        Picture p2 = pics.get(i);
+        System.out.println(p2.pictureName+" "+p2.toUser.getEmail()+p2.toUser.getFullName()+" ");
     }
-  }
-  public void removeFriend(LinkedList<User> u2, int i,User friend){
-    synchronized(this){
-      if(u2 == null || i >= u2.size()){
-        return;
-      }
-      User p = u2.get(i);
-      int id = p.getUserId();
-      if(id == friend.getUserId()){
-        u2.remove(i);
-        map1.put(id, u2);
-        p.userId = 0;
-        return;
-      }
-      removeFriend(u2, i+1,friend);
-    }
-  }
-  public void removeUser(User u){
-    synchronized(this){
-      if(!map.containsKey(u.getUserId())){
-        return;
-      }
-      map.remove(u.getUserId());
-      map1.remove(u.getUserId());
-      u.userId = 0;
-   }
-  }
-  public LinkedList<User> friendList(User u1){
-    return !map1.containsKey(u1.userId) ? null : map1.get(u1.userId);
+    System.out.println(" ------ ");
   }
 }
 public class Instagram {
   public static void main(String args[]){
-    User u1 = new User();
-    User u2 = new User("grk","Ganapathy","H","grk@gmail.com");
-    User u3 = new User("grk1","Ganesh","H","grk1@gmail.com");
-    User u4 = new User("grk2","Ramesh","H","grk2@gmail.com");
-    u1.addUser(u2);
-    u1.addUser(u3);
-    u1.addUser(u4);
-    u1.addFriend(u2,u3);
-    u1.addFriend(u2,u4);
-    u1.addPicture(u2, new Picture("Zend","Desktop/Zend.jpg"));
-    u1.addPicture(u2, new Picture("Zend1","Desktop/Zend1.jpg"));
-    u1.getPictures(u2);
+    InstagramSystem insSystem = new InstagramSystem();
+    insSystem.addUser("srupesh51@gmail.com","Rupesh S");
+    insSystem.addUser("srupesh52@gmail.com","Rupesh K");
+    insSystem.addUser("srupesh53@gmail.com","Rupesh M");
+    User u2 = insSystem.getUser("srupesh51@gmail.com");
+    User u3 = insSystem.getUser("srupesh52@gmail.com");
+    User u4 = insSystem.getUser("srupesh53@gmail.com");
+    insSystem.addPicture(u2, "PIC 1");
+    insSystem.addPicture(u3, "PIC 2");
+    insSystem.addPicture(u4, "PIC 3");
+    insSystem.showPicture(u2);
+    insSystem.showPicture(u3);
+    insSystem.showPicture(u4);
   }
 }
