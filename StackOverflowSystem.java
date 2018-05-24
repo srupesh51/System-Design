eec5acaf04f239df32f2eb9c26f227a93439d1c7
Rupesh <srupesh51@gmail.com>
2018-05-24 15:23:26 +0530

    Modified OOD for Stack Overflow System logic on System Design(Low Level)

--- StackOverflowSystem.java
+++ StackOverflowSystem.java
@@ -2,10 +2,13 @@ import java.util.*;
 class userAccount {
   private static HashMap<String, User> map = new HashMap<String, User>();
   userAccount(){}
-  public void createUserAccount(String email, String firstName, String lastName){
+  public void createUserAccount(String email, String firstName, String lastName, String technologies[]){
     if(!map.containsKey(email)){
-      map.put(email,new User(email,firstName,lastName));
+      map.put(email,new User(email,firstName,lastName,technologies));
     }
+  }
+  public boolean containsUser(String email){
+      return map.containsKey(email);
   }
   public User getUser(String email){
     if(map.containsKey(email)){
@@ -15,49 +18,41 @@ class userAccount {
   }
 }
 class Question {
-  private static HashMap<String,LinkedList<HashMap<String,LinkedList<Answer>>>> map = new HashMap<String,LinkedList<HashMap<String,LinkedList<Answer>>>>();
+  private String query;
+  private static HashMap<String,LinkedList<Answer>> map = new HashMap<String,LinkedList<Answer>>();
   Question(){}
-  public void addAnswer(String email, String query, String answer){
-    LinkedList<Answer> q = new LinkedList<Answer>();
-    HashMap<String,LinkedList<Answer>> ansL = new HashMap<String,LinkedList<Answer>>();
-    if(ansL == null){
-      ansL = new HashMap<String,LinkedList<Answer>>();
-    }
-    q.add(new Answer(answer));
-    ansL.put(email, q);
-    LinkedList<HashMap<String,LinkedList<Answer>>> li = new LinkedList<HashMap<String,LinkedList<Answer>>>();
+  public void addAnswer(User user, String query, String answer){
+    LinkedList<Answer> ans = null;
     if(!map.containsKey(query)){
-      li.add(ansL);
-      map.put(query, li);
+      ans = new LinkedList<Answer>();
+      ans.add(new Answer(user, answer));
     } else {
-      li = map.get(query);
-      li.add(ansL);
-      map.put(query, li);
+      ans = map.get(query);
+      ans.add(new Answer(user, answer));
     }
+    map.put(query, ans);
   }
   public void getAnswer(String query){
-    LinkedList<HashMap<String,LinkedList<Answer>>> ansL = map.get(query);
-    if(ansL == null){
-      System.out.println(" Sorry no results available ");
+    LinkedList<Answer> ans = map.get(query);
+    if(ans == null){
+      System.out.println("No results available ");
       return;
     }
-    System.out.println(" View the answers "+" ");
-    for(int j = 0; j < ansL.size(); j++){
-      HashMap<String,LinkedList<Answer>> map1 = ansL.get(j);
-      for(String key :  map1.keySet()){
-        LinkedList<Answer> ansS = map1.get(key);
-        System.out.println(" User "+key+" answered this question");
-        for(int i = 0; i < ansS.size(); i++){
-          System.out.println(" This answer "+ansS.get(i).getAnswer()+" has been answered "+" ");
-        }
-      }
+    for(int i = 0; i < ans.size(); i++){
+      Answer an = ans.get(i);
+      System.out.println(" User "+an.getUser().getEmail()+" answered this question "+an.getAnswer()+" ");
     }
   }
 }
 class Answer {
   private String answer;
-  Answer(String answer){
+  private User user;
+  Answer(User user, String answer){
+    this.user = user;
     this.answer = answer;
+  }
+  public User getUser(){
+    return this.user;
   }
   public String getAnswer(){
     return this.answer;
@@ -65,52 +60,65 @@ class Answer {
 }
 class StackOverflow {
   private static userAccount us = new userAccount();
-  private static HashMap<String,HashMap<String,LinkedList<Question>>> map = new HashMap<String,HashMap<String,LinkedList<Question>>>();
+  private static HashMap<String,LinkedList<User>> map = new HashMap<String,LinkedList<User>>();
   private static Question qsk = new Question();
   StackOverflow(){}
-  public void addUser(String email, String firstName, String lastName){
-    us.createUserAccount(email,firstName,lastName);
+  public void addUser(String email, String firstName, String lastName, String technologies[]){
+    us.createUserAccount(email,firstName,lastName,technologies);
   }
-  public void addQuestion(String email,String query,String technologies[]){
-    LinkedList<Question> q = new LinkedList<Question>();
-    for(int i = 0; i < technologies.length; i++){
-      HashMap<String,LinkedList<Question>> qL = map.get(query);
-      if(qL == null){
-        qL = new HashMap<String,LinkedList<Question>>();
-      }
-      q.add(new Question());
-      qL.put(email, q);
-      map.put(query, qL);
-    }
-  }
-  public void addAnswer(String email,String query,String answer){
-    qsk.addAnswer(email,query,answer);
-  }
-  public void showAnswers(String email,String query){
-    HashMap<String,LinkedList<Question>> qs = this.getQuestion(query);
-    if(qs == null){
+  public void addQuestion(String email,String query){
+    if(!us.containsUser(email)){
       return;
     }
-    System.out.println(" Displaying  answers for "+email+" for Question "+query+" ");
-    for(String key : qs.keySet()){
-      qsk.getAnswer(query);
+    User currentUser = us.getUser(email);
+    LinkedList<User> usL = null;
+    if(!map.containsKey(query)){
+      usL = new LinkedList<User>();
+      usL.add(currentUser);
+    } else {
+      usL = map.get(query);
+      usL.add(currentUser);
     }
+    map.put(query, usL);
   }
-  public HashMap<String,LinkedList<Question>> getQuestion(String query){
-    if(map.containsKey(query)){
-      return map.get(query);
+  public void addAnswer(String email,String query,String answer){
+    if(!us.containsUser(email)){
+      return;
     }
-    return null;
+    if(!this.containsQuestion(query)){
+      return;
+    }
+    User currentUser = us.getUser(email);
+    qsk.addAnswer(currentUser,query,answer);
+  }
+  public void showAnswers(String email,String query){
+    if(!us.containsUser(email)){
+      return;
+    }
+    if(!this.containsQuestion(query)){
+      return;
+    }
+    User currentUser = us.getUser(email);
+    System.out.println(" Hey User "+currentUser.getEmail()+" please view the responses for the Question "+query+" ");
+    qsk.getAnswer(query);
+  }
+  public boolean containsQuestion(String query){
+    return map.containsKey(query);
   }
 }
 class User {
   private String email;
   private String firstName;
   private String lastName;
-  User(String email, String firstName, String lastName){
+  private String[] technologies;
+  User(String email, String firstName, String lastName, String technologies[]){
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.technologies = technologies;
+  }
+  public String[] getTechnologies(){
+    return this.technologies;
   }
   public String getEmail(){
     return this.email;
@@ -127,10 +135,25 @@ public class StackOverflowSystem {
   public static void main(String args[]){
     StackOverflow so = new StackOverflow();
     String technologies[] = {"C","C++","Java"};
-    so.addQuestion("srupesh51@gmail.com","What is C++",technologies);
-    so.addAnswer("srupesh52@gmail.com","What is C++"," C++ is an object Oriented Language ");
-    so.addAnswer("srupeshsubra@gmail.com","What is C++"," C++ is an object Oriented Language and as well as standalone programming language ");
-    so.showAnswers("srupesh51@gmail.com","What is C++");
+    so.addUser("srupesh51@gmail.com","Rupesh","S",technologies);
+    so.addUser("srupesh52@gmail.com","Rupesh","S",technologies);
+    so.addUser("srupesh53@gmail.com","Rupesh","S",technologies);
+    so.addUser("srupesh54@gmail.com","Rupesh","S",technologies);
+    so.addUser("srupesh55@gmail.com","Rupesh","S",technologies);
+    so.addQuestion("srupesh51@gmail.com"," What is C++ ");
+    so.addQuestion("srupesh52@gmail.com"," What is C++ ");
+    so.addQuestion("srupesh51@gmail.com"," What is C ");
+    so.addQuestion("srupesh52@gmail.com"," What is C ");
+    so.addAnswer("srupesh53@gmail.com"," What is C++ ","Object Oriented and procedural language");
+    so.addAnswer("srupesh54@gmail.com"," What is C++ ","Object Oriented and functional language");
+    so.addAnswer("srupesh55@gmail.com"," What is C++ ","Object Oriented language");
+    so.addAnswer("srupesh53@gmail.com"," What is C ","procedural language");
+    so.addAnswer("srupesh54@gmail.com"," What is C ","programming language");
+    so.addAnswer("srupesh55@gmail.com"," What is C ","functional language");
+    so.showAnswers("srupesh51@gmail.com"," What is C++ ");
+    so.showAnswers("srupesh52@gmail.com", " What is C++ ");
+    so.showAnswers("srupesh51@gmail.com", " What is C ");
+    so.showAnswers("srupesh52@gmail.com", " What is C ");
   }
 
 }
